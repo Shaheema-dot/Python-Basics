513e043d758d2e10df4d9fdac6f0b45753c868fe
Shaheema Shaikh <59995290+Shaheema-dot@users.noreply.github.com>
2023-07-19 16:28:18 +0530

    Add files via upload

---
+++ if-else.py
@@ -0,0 +1,11 @@
+age = int(input("A="))
+if age <= 10:
+    print("child")
+else:
+    if age<= 20:
+        print("teenage")
+    else:
+        if age<= 40:
+            print("adult") 
+        else:
+            print("old")       
