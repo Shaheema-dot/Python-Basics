d7bf4027312a18c21c437016f0a685c06416ab51
Shaheema Shaikh <59995290+Shaheema-dot@users.noreply.github.com>
2023-07-19 17:41:05 +0530

    Add files via upload

---
+++ temperture.py
@@ -0,0 +1,8 @@
+Fahrenheit=int(input(" "))
+Celsius = (Fahrenheit - 32) * 5/9
+print("%d Fahrenheit = %.1f Celsius"%(Fahrenheit,Celsius))
+
+#celsius = float(input("Enter temperature in celsius: "))
+#fahrenheit = (celsius * 1.8) + 32
+#print(str(celsius )+ " degree Celsius is equal to " + str(fahrenheit )+ " degree Fahrenheit.")
+
